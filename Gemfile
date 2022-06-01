45fe8f0d46fd3d456efe70cbbe7cf3d24371c1d3
Ishotihadus <hanachan.pao@gmail.com>
2022-06-01 23:16:21 +0900

    first commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in google_cloud_logging_extension.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
---
+++ lib/google_cloud_logging_extension.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "google_cloud_logging_extension/version"
+
+module GoogleCloudLoggingExtension
+  class Error < StandardError; end
+  # Your code goes here...
+end
